141d8d40ab263ceb76f76aa1d3f36211d89e63a6
smsaifurrahman <smsaifur525@gmail.com>
2024-08-16 10:03:57 +0200

    Products page is created

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,11 +1,36 @@
+/** @format */
 
+import { useQuery } from "@tanstack/react-query";
+import { data } from "autoprefixer";
+import useAxiosPublic from "../hooks/useAxiosPublic";
+import ProductCard from "../Components/ProductCard";
 
 const Products = () => {
-    return (
-        <div>
-            Products
-        </div>
-    );
+   const axiosPublic = useAxiosPublic();
+   const {
+      data: products = [],
+      isLoading,
+      refetch,
+   } = useQuery({
+      queryKey: ["products"],
+      queryFn: async () => {
+         const { data } = await axiosPublic(`/products`);
+         //   setProductCount(data.length)
+         return data;
+      },
+   });
+   return (
+      <div>
+         <h1 className="text text-3xl text-center my-8 font-bold">
+            Product Finder Products
+         </h1>
+         <div className=" px-2 md:px-0 grid grid-cols-1 md:grid-cols-3 gap-6">
+            {products.map((product) => (
+               <ProductCard key={product.id} product={product}></ProductCard>
+            ))}
+         </div>
+      </div>
+   );
 };
 
 export default Products;
--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,19 @@ import "./index.css";
 import router from "./Routes/Routes.jsx";
 import AuthProvider from "./providers/AuthProvider.jsx";
 
+import {
+   QueryClient,
+   QueryClientProvider,
+   useQuery,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
    <StrictMode>
+      <QueryClientProvider client={queryClient}>
       <AuthProvider>
          <RouterProvider router={router} />
       </AuthProvider>
+      </QueryClientProvider>
    </StrictMode>
 );
